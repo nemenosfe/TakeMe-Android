feca1bc05c9914ff151af23dd85864f298e9a4d3
Oscar <oscar.serrano7@gmail.com>
2016-10-22 20:25:01 +0200

    Estructura de packages

--- app/src/main/java/com/pes/takemelegends/Activity/LoginActivity.java
+++ app/src/main/java/com/pes/takemelegends/Activity/LoginActivity.java
@@ -1,13 +1,12 @@
-package com.pes.takemelegends;
+package com.pes.takemelegends.Activity;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.content.Context;
+
+import com.pes.takemelegends.R;
 
 public class LoginActivity extends Activity implements View.OnClickListener {
 
--- app/src/main/java/com/pes/takemelegends/Activity/MainActivity.java
+++ app/src/main/java/com/pes/takemelegends/Activity/MainActivity.java
@@ -1,9 +1,7 @@
-package com.pes.takemelegends;
+package com.pes.takemelegends.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,7 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import static com.pes.takemelegends.R.color.white;
+import com.pes.takemelegends.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
