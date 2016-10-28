f831dd4ada84de4e17fcb280116ac94d81b5c3b2
Oscar <oscar.serrano7@gmail.com>
2016-10-28 14:42:25 +0200

    Pages

--- app/src/main/java/com/pes/takemelegends/Fragment/AsistireFragment.java
+++ app/src/main/java/com/pes/takemelegends/Fragment/AsistireFragment.java
@@ -3,10 +3,14 @@ package com.pes.takemelegends.Fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.pes.takemelegends.Adapter.EventAdapter;
 import com.pes.takemelegends.R;
 
 /**
@@ -19,12 +23,27 @@ public class AsistireFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_asistire, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_asistire, container, false);
+
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.totsRecyclerView);
+        linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        String[] dummy = {"Festival", "BOOM Festival 2016","Portugal", "16/10/2016 - 20:45h"};
+        EventAdapter totsAdapter = new EventAdapter(dummy);
+
+        recyclerView.setAdapter(totsAdapter);
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        return rootView;
     }
 
 }
