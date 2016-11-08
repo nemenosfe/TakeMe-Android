106ad83db3a3863ca4095c196109c77e6ee80ae3
Victor Gallego <victor.gallego.betorz@est.fib.upc.edu>
2016-11-08 14:56:38 +0100

    share working

--- app/src/main/java/com/pes/takemelegends/Activity/EventDetailsActivity.java
+++ app/src/main/java/com/pes/takemelegends/Activity/EventDetailsActivity.java
@@ -9,27 +9,29 @@ import android.widget.ImageButton;
 
 import com.pes.takemelegends.R;
 
-public class EventDetailsActivity extends Activity {
+public class EventDetailsActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
 
-
         ImageButton buttonShare = (ImageButton)findViewById(R.id.buttonShare);
-        buttonShare.setOnClickListener((View.OnClickListener) this);
+        buttonShare.setOnClickListener(this);
     }
 
+    @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case  R.id.buttonShare:
             {
                 Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
                 sharingIntent.setType("text/plain");
-                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "AndroidSolved");
-                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, "Now Learn Android with AndroidSolved clicke here to visit https://androidsolved.wordpress.com/ ");
-                startActivity(Intent.createChooser(sharingIntent, "Share via"));
+                String shareBody = "Mira este evento que he encontrado en Take Me Legends! www.takeme-legends.com";
+                String shareSub = "Take Me Legends!";
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSub);
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "Share using"));
 
                 break;
             }
