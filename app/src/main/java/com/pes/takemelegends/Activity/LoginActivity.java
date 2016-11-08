65ab368b43a135cb5101585d5aef5e2aee2225a4
xavier <xavier.algarra@est.fib.upc.edu>
2016-11-08 15:08:39 +0100

    Fabric + login twitter

--- app/src/main/java/com/pes/takemelegends/Activity/LoginActivity.java
+++ app/src/main/java/com/pes/takemelegends/Activity/LoginActivity.java
@@ -3,30 +3,82 @@ package com.pes.takemelegends.Activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.pes.takemelegends.R;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
-public class LoginActivity extends Activity implements View.OnClickListener {
+import io.fabric.sdk.android.Fabric;
+
+import static android.R.attr.data;
+
+public class LoginActivity extends Activity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "3BHl3HDnSPuOEz4h8udLjZ1an";
+    private static final String TWITTER_SECRET = "gCwOn4hM5xiC07KJXpONWZUw0moyz5OhP92GYFZRxveCImCtBK";
+    private TwitterLoginButton loginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_login);
 
 
-        Button buttonFacebook = (Button)findViewById(R.id.button_facebook);
-        buttonFacebook.setOnClickListener(this);
+        Button buttonFacebook = (Button) findViewById(R.id.button_facebook);
+        //buttonFacebook.setOnClickListener(this);
 
-        Button buttonGoogle = (Button)findViewById(R.id.button_google);
-        buttonGoogle.setOnClickListener(this);
+        Button buttonGoogle = (Button) findViewById(R.id.button_google);
+        //buttonGoogle.setOnClickListener(this);
 
-        Button buttonTwitter = (Button)findViewById(R.id.button_twitter);
-        buttonTwitter.setOnClickListener(this);
+        Button buttonTwitter = (Button) findViewById(R.id.twitter_login_button);
+        //buttonTwitter.setOnClickListener(this);
+
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // The TwitterSession is also available through:
+                // Twitter.getInstance().core.getSessionManager().getActiveSession()
+                TwitterSession session = result.data;
+                // TODO: Remove toast and use the TwitterSession's userID
+                // with your app's user model
+                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d("TwitterKit", "Login with Twitter failure", exception);
+            }
+        });
     }
 
-    @Override
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            // Make sure that the loginButton hears the result from any
+            // Activity that it triggered.
+            loginButton.onActivityResult(requestCode, resultCode, data);
+        }
+
+
+    }
+
+    /*@Override
     public void onClick(View view) {
         switch (view.getId()) {
             case  R.id.button_facebook:
@@ -41,12 +93,12 @@ public class LoginActivity extends Activity implements View.OnClickListener {
                 break;
             }
 
-            case R.id.button_twitter: {
+            case R.id.twitter_login_button: {
                 break;
             }
         }
 
-        /*Context context = this;
+        Context context = this;
         new AlertDialog.Builder(context)
                 .setTitle("Login")
                 .setMessage("Are you sure you want to login?")
@@ -66,5 +118,3 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         
                 .show();*/
 
-    }
-}
