33f349036af2b9c0603e006057d3ae006743beda
Victor Gallego <victor.gallego.betorz@est.fib.upc.edu>
2016-11-08 14:33:04 +0100

    share in progress

--- app/src/main/java/com/pes/takemelegends/Activity/EventDetailsActivity.java
+++ app/src/main/java/com/pes/takemelegends/Activity/EventDetailsActivity.java
@@ -1,7 +1,11 @@
 package com.pes.takemelegends.Activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.pes.takemelegends.R;
 
@@ -11,5 +15,24 @@ public class EventDetailsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
+
+
+        ImageButton buttonShare = (ImageButton)findViewById(R.id.buttonShare);
+        buttonShare.setOnClickListener((View.OnClickListener) this);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case  R.id.buttonShare:
+            {
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "AndroidSolved");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, "Now Learn Android with AndroidSolved clicke here to visit https://androidsolved.wordpress.com/ ");
+                startActivity(Intent.createChooser(sharingIntent, "Share via"));
+
+                break;
+            }
+        }
     }
 }
