4ef9a03a45f2386e9bb688828d57fd619962fc3e
Oscar <oscar.serrano7@gmail.com>
2016-11-08 15:37:25 +0100

    Market per no petar

--- app/src/main/java/com/pes/takemelegends/Activity/MarketPerLevelActivity.java
+++ app/src/main/java/com/pes/takemelegends/Activity/MarketPerLevelActivity.java
@@ -3,10 +3,17 @@ package com.pes.takemelegends.Activity;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.pes.takemelegends.Adapter.MarketAdapter;
+import com.pes.takemelegends.Adapter.MarketPerLevelAdapter;
+import com.pes.takemelegends.Assets.DividerItemDecorator;
 import com.pes.takemelegends.R;
 
 /**
@@ -20,12 +27,38 @@ public class MarketPerLevelActivity extends Fragment {
     }
 
     private View rootview;
+    private TextView userTV, lvlTV, takesTV;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         rootview = inflater.inflate(R.layout.fragment_market_per_level, container, false);
+
+        userTV = (TextView) rootview.findViewById(R.id.username);
+        lvlTV = (TextView) rootview.findViewById(R.id.currentLvl);
+        takesTV = (TextView) rootview.findViewById(R.id.totalTakes);
+        recyclerView = (RecyclerView) rootview.findViewById(R.id.logrosRecyclerView);
+
+        linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        DividerItemDecorator dividerItemDecoration = new DividerItemDecorator(recyclerView.getContext(),
+                linearLayoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        String[] dummy = {"Clauer RCDE", "description del cauer rcde", "300 takes"};
+        MarketPerLevelAdapter perLvlAdapter = new MarketPerLevelAdapter(dummy);
+
+        recyclerView.setAdapter(perLvlAdapter);
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        userTV.setText("oscarSeGa");
+        lvlTV.setText("Nivel 3");
+        takesTV.setText("777 takes");
 
         return rootview;
     }
--- app/src/main/java/com/pes/takemelegends/Adapter/MarketPerLevelAdapter.java
+++ app/src/main/java/com/pes/takemelegends/Adapter/MarketPerLevelAdapter.java
@@ -1,8 +1,53 @@
 package com.pes.takemelegends.Adapter;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.pes.takemelegends.R;
+
 /**
  * Created by Oscar on 08/11/2016.
  */
 
-public class MarketPerLevelAdapter {
+public class MarketPerLevelAdapter extends RecyclerView.Adapter<MarketPerLevelAdapter.ViewHolder> {
+
+    private String[] itemsData;
+
+    public MarketPerLevelAdapter(String[] itemsData) {
+        this.itemsData = itemsData;
+    }
+
+    @Override
+    public MarketPerLevelAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemLayoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.market_per_lvl_row, parent, false);
+        MarketPerLevelAdapter.ViewHolder viewHolder = new MarketPerLevelAdapter.ViewHolder(itemLayoutView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MarketPerLevelAdapter.ViewHolder viewHolder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemsData.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView level;
+        public ImageView lock;
+
+        public ViewHolder(View itemLayoutView) {
+            super(itemLayoutView);
+            level = (TextView) itemLayoutView.findViewById(R.id.lvl);
+            lock = (ImageView) itemLayoutView.findViewById(R.id.lock);
+        }
+    }
+
 }
