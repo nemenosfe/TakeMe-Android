3998e9fa9cbb1c0c71df27a91dc74a5760963573
Oscar <oscar.serrano7@gmail.com>
2016-11-11 15:45:01 +0100

    Main limpio

---
+++ app/src/main/java/com/pes/takemelegends/Activity/RewardsActivity.java
@@ -0,0 +1,15 @@
+package com.pes.takemelegends.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pes.takemelegends.R;
+
+public class RewardsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rewards);
+    }
+}
