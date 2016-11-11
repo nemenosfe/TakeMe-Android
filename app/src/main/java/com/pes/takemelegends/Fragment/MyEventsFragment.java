0bee725d63c8fcec2eed5facfb8e7b7fd417a745
Victor Gallego <victor.gallego.betorz@est.fib.upc.edu>
2016-11-11 15:45:24 +0100

    starting my events

---
+++ app/src/main/java/com/pes/takemelegends/Fragment/MyEventsFragment.java
@@ -0,0 +1,67 @@
+package com.pes.takemelegends.Fragment;
+
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pes.takemelegends.Adapter.EventPageAdapter;
+import com.pes.takemelegends.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MyEventsFragment extends Fragment {
+
+
+    public MyEventsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_my_events, container, false);
+
+        //TabLayout
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.text_historic));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.text_check_in));
+        tabLayout.setSelectedTabIndicatorColor(getResources().getColor(R.color.main_ambar));
+        tabLayout.setSelectedTabIndicatorHeight(5);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) view.findViewById(R.id.pager);
+        final PagerAdapter adapter = new EventPageAdapter(
+                getChildFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        return view;
+    }
+
+}
