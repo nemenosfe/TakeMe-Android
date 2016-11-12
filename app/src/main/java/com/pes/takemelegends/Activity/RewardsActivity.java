7a3785f4778cd38e65c41ec64908a27225f674ea
Oscar <oscar.serrano7@gmail.com>
2016-11-12 10:27:25 +0100

    Market realizado

--- app/src/main/java/com/pes/takemelegends/Activity/RewardsActivity.java
+++ app/src/main/java/com/pes/takemelegends/Activity/RewardsActivity.java
@@ -2,14 +2,47 @@ package com.pes.takemelegends.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
+import com.pes.takemelegends.Adapter.MarketPerLevelAdapter;
+import com.pes.takemelegends.Assets.DividerItemDecorator;
 import com.pes.takemelegends.R;
 
 public class RewardsActivity extends AppCompatActivity {
+
+    private TextView userTV, lvlTV, takesTV;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rewards);
+
+        userTV = (TextView) findViewById(R.id.username);
+        lvlTV = (TextView) findViewById(R.id.currentLvl);
+        takesTV = (TextView) findViewById(R.id.totalTakes);
+        recyclerView = (RecyclerView) findViewById(R.id.rewardsRecyclerView);
+
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        DividerItemDecorator dividerItemDecoration = new DividerItemDecorator(recyclerView.getContext(),
+                linearLayoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        String[] dummy = {"Clauer RCDE", "description del cauer rcde", "300 takes"};
+        MarketPerLevelAdapter perLvlAdapter = new MarketPerLevelAdapter(dummy);
+
+        recyclerView.setAdapter(perLvlAdapter);
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        userTV.setText("oscarSeGa");
+        lvlTV.setText("Nivel 3");
+        takesTV.setText("777 takes");
     }
 }
--- app/src/main/java/com/pes/takemelegends/Adapter/MarketAdapter.java
+++ app/src/main/java/com/pes/takemelegends/Adapter/MarketAdapter.java
@@ -1,5 +1,6 @@
 package com.pes.takemelegends.Adapter;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -10,6 +11,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.pes.takemelegends.Activity.RewardsActivity;
 import com.pes.takemelegends.R;
 
 public class MarketAdapter extends RecyclerView.Adapter<MarketAdapter.ViewHolder>  {
@@ -50,9 +52,11 @@ public class MarketAdapter extends RecyclerView.Adapter<MarketAdapter.ViewHolder
 
         public TextView level;
         public ImageView lock;
+        private final Context context;
 
         public ViewHolder(View itemLayoutView) {
             super(itemLayoutView);
+            context = itemLayoutView.getContext();
             level = (TextView) itemLayoutView.findViewById(R.id.lvl);
             lock = (ImageView) itemLayoutView.findViewById(R.id.lock);
             itemLayoutView.setOnClickListener(this);
@@ -61,7 +65,9 @@ public class MarketAdapter extends RecyclerView.Adapter<MarketAdapter.ViewHolder
 
         @Override
         public void onClick(View view) {
-            Log.v("asdfasdf", String.valueOf(getLayoutPosition()));
+            Intent intent = new Intent(context, RewardsActivity.class);
+            intent.putExtra("id", getLayoutPosition());
+            context.startActivity(intent);
         }
 
 
