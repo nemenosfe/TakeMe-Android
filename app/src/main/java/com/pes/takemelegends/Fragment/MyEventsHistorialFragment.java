a6daf8ff83f90ee2e991c537e4c2b1ee52c70c30
Victor Gallego <victor.gallego.betorz@est.fib.upc.edu>
2016-11-15 14:55:53 +0100

    image

---
+++ app/src/main/java/com/pes/takemelegends/Fragment/MyEventsHistorialFragment.java
@@ -0,0 +1,47 @@
+package com.pes.takemelegends.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pes.takemelegends.Adapter.EventAdapter;
+import com.pes.takemelegends.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MyEventsHistorialFragment extends Fragment {
+
+
+    public MyEventsHistorialFragment() {
+        // Required empty public constructor
+    }
+
+
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_tots_events, container, false);
+
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.totsRecyclerView);
+        linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        String[] dummy = {"Festival", "BOOM Festival 2016","Portugal", "16/10/2016 - 20:45h"};
+        EventAdapter totsAdapter = new EventAdapter(dummy, getActivity());
+
+        recyclerView.setAdapter(totsAdapter);
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        return rootView;
+    }
+
+}
