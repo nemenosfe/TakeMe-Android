8959e277d471932c9b5d0c91340fab55921e9967
Jaume Lladó <jaume.llado.avellaneda@est.fib.upc.edu>
2016-12-02 15:16:31 +0100

    APP_KEY in resources

--- app/src/main/java/com/pes/takemelegends/Utils/URLResources.java
+++ app/src/main/java/com/pes/takemelegends/Utils/URLResources.java
@@ -2,6 +2,7 @@ package com.pes.takemelegends.Utils;
 
 public class URLResources {
     private static final String BASE_URL = "http://10.4.41.167:8888";
+    public static final String APP_KEY = "7384d85615237469c2f6022a154b7e2c";
     public static final String ACHIEVEMENTS_URL = BASE_URL + "/achievements";
     public static final String USER_ACHIEVEMENTS_URL = ACHIEVEMENTS_URL + "/user";
     public static final String EVENTS_URL = BASE_URL + "/events";
