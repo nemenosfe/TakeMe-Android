319739985ceacff89bff56da4a72397a9e264b0c
Oscar <oscar.serrano7@gmail.com>
2016-12-02 15:50:05 +0100

    getters del controlador devents creat

--- app/src/main/java/com/pes/takemelegends/Controller/EventController.java
+++ app/src/main/java/com/pes/takemelegends/Controller/EventController.java
@@ -19,10 +19,16 @@ public class EventController {
     public void getEventInfo(AsyncHttpResponseHandler responseHandler, String id) {
         RequestParams params = new RequestParams();
         params.add("id", id);
+        params.add("appkey", URLResources.APP_KEY);
         client.get(URLResources.EVENTS_URL, responseHandler);
     }
 
-    public void getEventsUser(AsyncHttpResponseHandler responseHandler, String id)
+    public void getEventsUser(AsyncHttpResponseHandler responseHandler, String token) {
+        RequestParams params = new RequestParams();
+        params.add("id", token);
+        params.add("appkey", URLResources.APP_KEY);
+        client.get(URLResources.EVENTS_URL, responseHandler);
+    }
 
     public void getEventsByCategory(AsyncHttpResponseHandler responseHandler, String category) {
         RequestParams params = new RequestParams("category", category);
