b6021b4350715ba6824ada7c44b260b42a0bc8dc
Marta Noya <marta.noya@est.fib.upc.edu>
2016-12-09 15:52:28 +0100

    Tests de Preferences en progrés

--- app/src/test/java/com/pes/takemelegends/UserController.java
+++ app/src/test/java/com/pes/takemelegends/UserController.java
@@ -1,8 +1,13 @@
 package com.pes.takemelegends;
 
-import com.loopj.android.http.AsyncHttpResponseHandler;
+import android.util.Log;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
 import com.pes.takemelegends.Controller.ControllerFactory;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.After;
 import org.junit.Test;
 
 import cz.msebera.android.httpclient.Header;
@@ -11,12 +16,50 @@ import static org.junit.Assert.*;
 
 public class UserController {
     private com.pes.takemelegends.Controller.UserController userController = ControllerFactory.getInstance().getUserController();
-    private String uid = "1", provider = "provTestAnd", token = "abcd";
+    private String uid = "1", provider = "provTestAnd", token = "abcd"; // Dades temporals
+    private JSONObject putCategories;
+
+    @Test
+    public void postPreferences() throws Exception {
+        userController.postPreferences(
+                new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        assertEquals(getJSONPreferences(null), response); // Temporal
+                    }
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                        try {
+                            fail(errorResponse.getString("message"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                uid,
+                provider,
+                token,
+                getJSONPreferences(null).toString()
+        );
+    }
 
     @Test
     public void getPreferences() throws Exception {
         userController.getPreferences(
-                jsonHttp,
+                new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        assertEquals(2+2, 4); // Temporal
+                    }
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                        try {
+                            fail(errorResponse.getString("message"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
                 uid,
                 provider,
                 token
@@ -24,13 +67,61 @@ public class UserController {
     }
 
     @Test
-    public void postPreferences() throws Exception {
-        //
+    public void putPreferences() throws Exception {
+        userController.putPreferences(
+                new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        assertEquals(2+2, 4); // Temporal
+                    }
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                        try {
+                            fail(errorResponse.getString("message"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                uid,
+                provider,
+                token,
+                getJSONPreferences(putCategories).toString()
+        );
     }
 
-    @Test
-    public void putPreferences() throws Exception {
-        //
+    private JSONObject getJSONPreferences(JSONObject newCategoryList) {
+        JSONObject preferences = new JSONObject();
+        try {
+            preferences.put("categories", (newCategoryList == null) ? "default!!!" : newCategoryList.toString());
+            preferences.put("locations", -1);
+            preferences.put("start_hour", -1);
+            preferences.put("end_hour", -1);
+            preferences.put("week", -1);
+            preferences.put("weekend", -1);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return preferences;
+    }
+
+    @After public void putOriginalValues() {
+        userController.putPreferences(
+                new JsonHttpResponseHandler() {
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                        try {
+                            Log.e("ErrorPutOriginalValues", errorResponse.getString("message"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                uid,
+                provider,
+                token,
+                getJSONPreferences(null).toString()
+        );
     }
 
 }
