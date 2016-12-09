37bc028488095e656e406bac4fd7d58a1ce8a9f2
xavier <xavier.algarra@est.fib.upc.edu>
2016-12-09 15:16:26 +0100

    SharedPreferencesManager con 3 atributos

---
+++ app/src/main/java/com/pes/takemelegends/Utils/SharedPreferencesManager.java
@@ -0,0 +1,98 @@
+package com.pes.takemelegends.Utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPreferencesManager {
+
+    public static final String PREFS_NAME = "TakeMeLegends";
+
+    private static String userUid = "";
+    private static String userToken = "";
+    private static String userProvider = "";
+    SharedPreferences sp;
+
+    /**
+     * creates a shared preferences controller with the actual appication context
+     * load the actual values.
+     * @param context actual application context
+     */
+    public SharedPreferencesManager(Context context) {
+        sp = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        userUid = (String) getObject("userId", String.class.getSimpleName());
+        userToken = (String) getObject("userToken", String.class.getSimpleName());
+        userProvider = (String) getObject("userRole", String.class.getSimpleName());
+    }
+
+    public String getUserToken() {
+        return userToken;
+    }
+
+    public void setUserToken(String userToken) {
+        this.userToken = userToken;
+        setValue("userToken",userToken);
+    }
+
+    public String getUserId() {
+        return userUid;
+    }
+    public void setUserId(String userId) {
+        this.userUid = userId;
+        setValue("userUid",userId);
+    }
+
+    public String getUserProvider() {
+        return userProvider;
+    }
+    public void setUserCompanyId(String userCompanyId) {
+        this.userProvider = userCompanyId;
+        setValue("userProvider",userCompanyId);
+    }
+
+
+    /**
+     *retrieves the object stored in the key
+     * @param key Key to recover the file
+     * @param classtype Descriptor to know the class of the object to find
+     * @return The value of the object searched or a default value
+     */
+    private Object getObject(String key, String classtype) {
+        switch (classtype) {
+            case "Boolean":
+                return sp.getBoolean(key,false);
+            case "Integer":
+                return sp.getInt(key, 0);
+            case "String":
+                return sp.getString(key, "");
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * test if value is boolean or int or string
+     * then save it to shared preference with param key
+     * @param key name of the shared preference value
+     * @param value object to save.
+     */
+    private void setValue(String key, Object value) {
+        SharedPreferences.Editor editor = sp.edit();
+        switch (value.getClass().getSimpleName()){
+            case "Boolean":
+                editor.putBoolean(key, (Boolean) value);
+                break;
+            case "Integer":
+                editor.putInt(key, (Integer) value);
+                break;
+            case "String":
+                editor.putString(key, (String) value);
+                break;
+        }
+        boolean t = editor.commit();
+
+        int c = 0;
+        int b = 2;
+        int s = c + b;
+
+    }
+}
