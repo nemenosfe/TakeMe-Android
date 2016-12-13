e395f4c169525d4d70b7a4d4ca954a71b130813e
Victor Gallego <victor.gallego.betorz@est.fib.upc.edu>
2016-12-13 15:52:01 +0100

    location

--- app/src/main/java/com/pes/takemelegends/Adapter/EventCheckInAdapter.java
+++ app/src/main/java/com/pes/takemelegends/Adapter/EventCheckInAdapter.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 
 import com.pes.takemelegends.Fragment.TotsEventsFragment;
 import com.pes.takemelegends.R;
+import com.pes.takemelegends.Utils.GPSTracker;
 
 /**
  * Created by victorgallegobetorz on 18/11/16.
@@ -49,6 +50,7 @@ public class EventCheckInAdapter extends RecyclerView.Adapter<EventCheckInAdapte
 
         private Context context;
         private View itemLayoutView;
+        private GPSTracker gps;
 
         ViewHolder(View itemLayoutView, Context context) {
             super(itemLayoutView);
@@ -61,7 +63,25 @@ public class EventCheckInAdapter extends RecyclerView.Adapter<EventCheckInAdapte
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(this.context, "asdf", Toast.LENGTH_SHORT).show();
+            //http://www.androidhive.info/2012/07/android-gps-location-manager-tutorial/
+            //Toast.makeText(this.context, "asdf", Toast.LENGTH_SHORT).show();
+
+            gps = new GPSTracker(this.context);
+
+            // check if GPS enabled
+            if(gps.canGetLocation()){
+
+                double latitude = gps.getLatitude();
+                double longitude = gps.getLongitude();
+
+                // \n is for new line
+                Toast.makeText(this.context, "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+            }else{
+                // can't get location
+                // GPS or Network is not enabled
+                // Ask user to enable GPS/network in settings
+                gps.showSettingsAlert();
+            }
 
         }
     }
