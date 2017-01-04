3dac2c070b22e77f914fe8a0890c1bafa5c868a9
Oscar <oscar.serrano7@gmail.com>
2017-01-04 13:19:18 +0100

    Historial d'events

--- app/src/main/java/com/pes/takemelegends/Adapter/EventHistorialAdapter.java
+++ app/src/main/java/com/pes/takemelegends/Adapter/EventHistorialAdapter.java
@@ -2,6 +2,7 @@ package com.pes.takemelegends.Adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.text.Spannable;
 import android.text.SpannableString;
@@ -18,17 +19,18 @@ import com.pes.takemelegends.Activity.EventDetailsActivity;
 import com.pes.takemelegends.R;
 import com.squareup.picasso.Picasso;
 
+import java.util.List;
+
 /**
  * Created by victorgallegobetorz on 15/11/16.
  */
 
 public class EventHistorialAdapter extends RecyclerView.Adapter<EventHistorialAdapter.ViewHolder> {
 
-    private String[] itemsData;
+    private List<String[]> itemsData;
     private Context context;
-    private final String imageUrl = "http://www.hutterites.org/wp-content/uploads/2012/03/placeholder.jpg";
 
-    public EventHistorialAdapter(String[] itemsData, Context context) {
+    public EventHistorialAdapter(List<String[]>  itemsData, Context context) {
 
         this.itemsData = itemsData;
         this.context = context;
@@ -37,31 +39,55 @@ public class EventHistorialAdapter extends RecyclerView.Adapter<EventHistorialAd
     @Override
     public EventHistorialAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemLayoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.event_histrorial_row, parent, false);
-        ViewHolder viewHolder = new ViewHolder(itemLayoutView);
-        return viewHolder;
+        return new ViewHolder(itemLayoutView);
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        String[] data = itemsData.get(position);
+        if (data[0].equals("1")) {
+            viewHolder.status.setText(context.getString(R.string.text_was_at_time)+"\nFalta hora en API!");
+            viewHolder.takes.setBackgroundColor(context.getResources().getColor(R.color.green));
+        }
+        else {
+            viewHolder.status.setText(context.getString(R.string.text_no_reward_obtained));
+            viewHolder.takes.setBackgroundColor(context.getResources().getColor(R.color.red));
+        }
+        viewHolder.eventName.setText(data[1]);
+        viewHolder.eventDesc.setText(data[2]);
+        viewHolder.eventDate.setText(data[3]);
+        viewHolder.takes.setText(data[4]+"\ntakes");
+        viewHolder.id.setText(data[5]);
     }
 
     @Override
     public int getItemCount() {
-        return itemsData.length;
+        return itemsData.size();
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-;
+        TextView takes, status, eventName, eventDesc, eventDate, id;
+        private final Context context;
 
         ViewHolder(View itemLayoutView) {
             super(itemLayoutView);
 
+            context = itemLayoutView.getContext();
+            takes = (TextView) itemLayoutView.findViewById(R.id.takes);
+            status = (TextView) itemLayoutView.findViewById(R.id.status);
+            eventName = (TextView) itemLayoutView.findViewById(R.id.eventName);
+            eventDesc = (TextView) itemLayoutView.findViewById(R.id.eventDesc);
+            eventDate = (TextView) itemLayoutView.findViewById(R.id.eventDate);
+            id = (TextView) itemLayoutView.findViewById(R.id.eventId);
+            itemLayoutView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View view) {
-
+            Intent intent = new Intent(context, EventDetailsActivity.class);
+            intent.putExtra("id", id.getText().toString());
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
         }
     }
 }
--- app/src/main/java/com/pes/takemelegends/Fragment/MyEventsHistorialFragment.java
+++ app/src/main/java/com/pes/takemelegends/Fragment/MyEventsHistorialFragment.java
@@ -1,7 +1,9 @@
 package com.pes.takemelegends.Fragment;
 
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,24 +11,42 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 
+import com.loopj.android.http.JsonHttpResponseHandler;
 import com.pes.takemelegends.Adapter.EventAdapter;
 import com.pes.takemelegends.Adapter.EventHistorialAdapter;
+import com.pes.takemelegends.Controller.ControllerFactory;
+import com.pes.takemelegends.Controller.EventController;
 import com.pes.takemelegends.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MyEventsHistorialFragment extends Fragment {
 
-
     public MyEventsHistorialFragment() {
         // Required empty public constructor
     }
 
-
     private RecyclerView recyclerView;
     private LinearLayoutManager linearLayoutManager;
+    private EventController eventController;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        eventController = ControllerFactory.getInstance().getEventController();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,10 +58,48 @@ public class MyEventsHistorialFragment extends Fragment {
         linearLayoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(linearLayoutManager);
 
-        String[] dummy = {"Festival", "BOOM Festival 2016","Portugal", "16/10/2016 - 20:45h"};
-        EventHistorialAdapter totsAdapter = new EventHistorialAdapter(dummy, getActivity());
+        final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("Obteniendo eventos");
+        progressDialog.setProgressStyle(android.app.ProgressDialog.STYLE_SPINNER);
+        progressDialog.setIndeterminate(true);
+        progressDialog.show();
+        progressDialog.setCancelable(false);
+        progressDialog.setCanceledOnTouchOutside(false);
+        eventController.getEventsUser(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                List<String[]> events = new ArrayList<>();
+                //Present
+                try {
+                    JSONObject present = response.getJSONObject("past");
+                    JSONArray eventArray = present.optJSONArray("events");
+                    for (int i = 0; i < eventArray.length(); i++) {
+                        JSONObject event = eventArray.getJSONObject(i).getJSONObject("event");
+                        if (event != null) {
+                            JSONObject cat = event.getJSONObject("categories");
+                            String checkin_done = event.isNull("checkin_done") ? "" : event.getString("checkin_done");
+                            String title = event.isNull("title") ? "" : event.getString("title");
+                            String description = event.isNull("description") ? "" : event.getString("description");
+                            String startTime = event.isNull("start_time") ? "" : event.getString("start_time");
+                            String id = event.getString("id");
+                            String takes = event.isNull("takes") ? "0" : String.valueOf(event.getInt("takes"));
+                            events.add(new String[]{checkin_done, title, description, startTime, takes, id});
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
-        recyclerView.setAdapter(totsAdapter);
+                EventHistorialAdapter historialAdapter = new EventHistorialAdapter(events, getActivity());
+                recyclerView.setAdapter(historialAdapter);
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                progressDialog.dismiss();
+            }
+        }, getActivity(), "50", null);
 
         recyclerView.setItemAnimator(new DefaultItemAnimator());
 
