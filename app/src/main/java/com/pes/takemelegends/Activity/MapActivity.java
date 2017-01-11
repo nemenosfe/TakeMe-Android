ddcdab746518ac6c94c1f04e75b5db40e4fc89ab
Marta Noya <marta.noya@est.fib.upc.edu>
2017-01-11 10:57:10 +0100

    OSCAR: ultims canvis

--- app/src/main/java/com/pes/takemelegends/Activity/MapActivity.java
+++ app/src/main/java/com/pes/takemelegends/Activity/MapActivity.java
@@ -5,9 +5,12 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.pes.takemelegends.Fragment.MapFragment;
 import com.pes.takemelegends.R;
@@ -36,6 +39,10 @@ public class MapActivity extends FragmentActivity {
         float latitude = b.getFloat("latitude");
         float longitude = b.getFloat("longitude");
         String address = b.getString("address");
+
+        TextView title = (TextView) findViewById(R.id.title);
+        title.setText(address);
+        title.setEllipsize(TextUtils.TruncateAt.END);
 
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         if (map == null) map = new MapFragment(latitude, longitude, address);
